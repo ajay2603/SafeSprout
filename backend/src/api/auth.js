7d1f2604edf1b92654d5061fa020208de1a64d55
ajay2603 <j.a.r.raju2003@gmail.com>
2024-02-08 22:12:48 +0530

    user signin and signup done.

---
+++ backend/src/api/auth.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+
+const { User } = require("../database/models");
+
+router.post("/login", async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    const usr = User.findOne({ userName: userName });
+    if (usr) {
+      if (usr.password == password) {
+        res.json({ stat: true });
+      } else {
+        res.json({ stat: true, err: false, usr: true });
+      }
+    } else {
+      res.json({ stat: false, err: false, usr: false });
+    }
+  } catch (err) {
+    console.log(err);
+    res.json({ stat: false, err: true });
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  const { userName, password, name, email } = req.body;
+
+  const usr = await User.findOne({ userName: userName });
+  if (usr) {
+    res.json({ stat: false, err: false });
+  } else {
+    const new_user = new User({
+      userName: userName,
+      password: password,
+      name: name,
+      email: email,
+    });
+    new_user
+      .save()
+      .then((resp) => {
+        res.json({ stat: true, userName: userName });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ stat: false, err: true });
+      });
+  }
+});
+
+module.exports = router;
---
+++ backend/src/database/models.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userName: String,
+  password: String,
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = { User: User };
